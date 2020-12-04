b47700e1258e1f3cd1df6f3ab3dbbed0493f2da7
Duhamster <Mm_aa_gg_ee@mail.ru>
2020-12-04 17:29:05 +0500

    Not-working-doors

--- 3rd Person Dungeon/Assets/Scripts/RoomController.cs
+++ 3rd Person Dungeon/Assets/Scripts/RoomController.cs
@@ -4,19 +4,89 @@ using UnityEngine;
 
 public class RoomController : MonoBehaviour
 {
+    private GameObject[] grids;
     private bool isPlayerHere;
     public GameObject _Enemies;
+    public bool enemiesSlayed;
+
+    public bool battle = false;
+    public bool cleaned = false;
 
     void Start()
     {
-        
+        grids = GameObject.FindGameObjectsWithTag("Grid");
     }
 
     void Update()
     {
         if (isPlayerHere)
         {
-            _Enemies.SetActive(true);
+            if (!cleaned)
+            {
+                battle = true;
+                _Enemies.SetActive(true);
+            }
+            if (battle == true)
+            {
+                foreach (GameObject i in grids)
+                {
+                    if (i.transform.position.y > 1)
+                    {
+                        i.transform.Translate(new Vector3(0, -3f, 0));
+                    }
+                }
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    enemiesSlayed = true;
+                    if (transform.GetChild(i).gameObject.activeInHierarchy)
+                    {
+                        enemiesSlayed = false;
+                    }
+                }
+                if (enemiesSlayed) {
+                    battle = false;
+                    cleaned = true;
+                        }
+            }
+            /*if (!enemiesSlayed)
+            {
+                foreach (GameObject i in grids)
+                {
+                    if (i.transform.position.y > 1)
+                    {
+                        i.transform.Translate(new Vector3(0, -3f, 0));
+                    }
+                }
+            }
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                enemiesSlayed = true;
+                if (transform.GetChild(i).gameObject.activeSelf)
+                {
+                    enemiesSlayed = false;
+                }
+            }
+            if(enemiesSlayed == true)
+            {
+                foreach (GameObject i in grids)
+                {
+                    if (i.transform.position.y < 4)
+                    {
+                        i.transform.Translate(new Vector3(0, 3f, 0));
+                    }
+                }
+            } */
+
+        }
+        else
+        {
+            foreach (GameObject i in grids)
+            {
+                if (i.transform.position.y < 4)
+                {
+                    i.transform.Translate(new Vector3(0, 3f, 0));
+                }
+            }
         }
     }
 
@@ -26,15 +96,16 @@ public class RoomController : MonoBehaviour
             enemy.GetComponentInChildren<MessengerScript>().WakeUp();
         }
     }*/
-
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player")
         {
             isPlayerHere = true;
+            
         }
         else
         {
+            
             isPlayerHere = false;
         }
     }
