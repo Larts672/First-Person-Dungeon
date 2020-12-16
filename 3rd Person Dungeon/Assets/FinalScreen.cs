3a6d5190bb3929bf681a0baa987b06445102a564
Larts <dima.esenin.1996@mail.ru>
2020-12-16 16:42:27 +0500

    Commit

---
+++ 3rd Person Dungeon/Assets/FinalScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinalScreen : MonoBehaviour
+{
+    public Text finalText;
+
+    void Start()
+    {
+        finalText = GameObject.Find("FinalText").GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
