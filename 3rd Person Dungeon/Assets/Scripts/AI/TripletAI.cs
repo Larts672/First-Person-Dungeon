116962455c3f04947d6270966e2dc8ffe8c3469b
Duhamster <Mm_aa_gg_ee@mail.ru>
2021-01-29 16:48:54 +0500

    Triplets working

--- 3rd Person Dungeon/Assets/Scripts/AI/TripletAI.cs
+++ 3rd Person Dungeon/Assets/Scripts/AI/TripletAI.cs
@@ -4,30 +4,53 @@ using UnityEngine;
 
 public class TripletAI : MonoBehaviour
 {
-    public Animator peaAnim; // Аниматор
+    public Animator tripletAnim; // Аниматор
     public GameObject target; // Цель (не "player", т.к. может будут миньоны)
     public float seeDistance = 50f; // Дальность видимости
-    public float rangedAttackDistance = 1500f; // Дальность атаки
+    public float maxRangedAttackDistance;
+    public float minRangedAttackDistance;
+    public float meleeAttackDistance;
     public float speed = 4f; // Скорость
     public float rotateSpeed = 2f;
 
+
     // Атака
-    public Transform attackPlace;
+    public GameObject leftAttackPlace;
+    public GameObject mediumAttackPlace;
+    public GameObject rightAttackPlace;
     public GameObject projectile;
-    public bool canAttack = true;
+
+    private bool canShoot;
+    private bool canBite;
     public float attackDelay = 1.5f;
     public Vector3 rotationVec;
     private float attackFixedDelay;
 
+    public float biteDelay = 1.5f;
+    private float fixedBiteDelay;
+
     void Start()
     {
         attackFixedDelay = attackDelay;
-        peaAnim = GetComponent<Animator>();
+        fixedBiteDelay = biteDelay;
+        tripletAnim = GetComponent<Animator>();
         target = GameObject.Find("Player");
     }
 
     void Update()
     {
+        if((Vector3.Distance(target.transform.position, transform.position) <= maxRangedAttackDistance))
+        {
+            canShoot = true;
+        }
+        else
+        {
+            canShoot = false;
+        }
+        if(Vector3.Distance(target.transform.position, transform.position) <= minRangedAttackDistance)
+        {
+            canShoot = false;
+        }
         Chase();
 
         Attack();
@@ -39,16 +62,43 @@ public class TripletAI : MonoBehaviour
         {
             attackDelay -= Time.deltaTime;
         }
-        if (Vector3.Distance(target.transform.position, transform.position) <= rangedAttackDistance && this.gameObject.GetComponent<EnemyHP>().hp > 0 && attackDelay < 0)
+        if (canShoot && this.gameObject.GetComponent<EnemyHP>().hp > 0&& attackDelay < 0)
         {
 
-                peaAnim.SetTrigger("Projectile Attack");
-                Instantiate(projectile, attackPlace.position, transform.rotation);
+                tripletAnim.SetTrigger("Projectile Attack");
+                Instantiate(projectile, leftAttackPlace.transform.position, leftAttackPlace.transform.rotation);
+                Instantiate(projectile, mediumAttackPlace.transform.position, mediumAttackPlace.transform.rotation);
+                Instantiate(projectile, rightAttackPlace.transform.position, rightAttackPlace.transform.rotation);
 
-                attackDelay = attackFixedDelay;
+            attackDelay = attackFixedDelay;
             
 
         }
+
+        if (Vector3.Distance(target.transform.position, transform.position) <= meleeAttackDistance && this.gameObject.GetComponent<EnemyHP>().hp > 0)
+        {
+            transform.rotation = Quaternion.LookRotation(new Vector3(target.transform.position.x - transform.position.x, 0f, target.transform.position.z - transform.position.z));
+
+            if (canBite)
+            {
+                tripletAnim.SetBool("Walk Forward", false);
+                tripletAnim.SetBool("Bite Attack", true);
+                //canBite = false;
+                biteDelay = Random.Range(1.7f, 2.9f);
+            }
+
+            if (!canBite)
+            {
+                biteDelay -= Time.deltaTime;
+                if (biteDelay <= 0)
+                {
+                    canBite = true;
+                }
+            }
+
+        }
+
+
     }
 
     private void Chase()
@@ -56,9 +106,9 @@ public class TripletAI : MonoBehaviour
         transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(new Vector3(target.transform.position.x - transform.position.x, 0f, target.transform.position.z - transform.position.z)), rotateSpeed / 100);
         //rotationVec = new Vector3(target.transform.position.x - transform.position.x, 0f, target.transform.position.z - transform.position.z);
         //transform.rotation = Quaternion.LookRotation(rotationVec*0.001f);
-        if (Vector3.Distance(target.transform.position, transform.position) <= seeDistance && Vector3.Distance(target.transform.position, transform.position) >= rangedAttackDistance && this.gameObject.GetComponent<EnemyHP>().hp > 0)
+        if (Vector3.Distance(target.transform.position, transform.position) <= seeDistance && !canShoot && (Vector3.Distance(target.transform.position, transform.position) >= meleeAttackDistance) && this.gameObject.GetComponent<EnemyHP>().hp > 0)
         {
-            peaAnim.SetBool("Walk Forward", true);
+            tripletAnim.SetBool("Walk Forward", true);
             transform.position = Vector3.MoveTowards
                 (transform.position,
                 new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z),
@@ -66,7 +116,7 @@ public class TripletAI : MonoBehaviour
         }
         else
         {
-            peaAnim.SetBool("Walk Forward", false);
+            tripletAnim.SetBool("Walk Forward", false);
         }
     }
 }
